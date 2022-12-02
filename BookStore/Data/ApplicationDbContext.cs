f0d4cbbbaccd43e1a79788c9448abd492f721d0a
viettrinh8412 <caothiviettrinh@gmail.com>
2022-12-02 16:36:20 +0700

    Inittial source

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
